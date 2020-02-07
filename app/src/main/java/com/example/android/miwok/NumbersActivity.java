0000f98941f554b774e48febe9c2c80b5be18aa7
Dima Usatyuk <Shonen1988@gmail.com>
2020-02-07 11:55:13 +0200

    added child TextView at parent LinearLayout in loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -24,5 +26,14 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumbersWords.add("Eight");
         englishNumbersWords.add("Nine");
         englishNumbersWords.add("Ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        int size = englishNumbersWords.size();
+        for (int i = 0; i < size; i++) {
+            TextView numbersWords = new TextView(this);
+            numbersWords.setText(englishNumbersWords.get(i));
+            rootView.addView(numbersWords,i);
+        }
     }
 }
