5e4f2e6874b2a6ff788b63d952a61916fb4115d6
Dima Usatyuk <Shonen1988@gmail.com>
2020-02-28 13:02:18 +0200

    add releasedMediaPlayer()method in Colors,Family.Phrases and Numbers classes to clear MP resource

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -14,33 +14,58 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
         final ArrayList<Word> numbersWords = new ArrayList<Word>();
-        numbersWords.add(new Word("lutti", "one", R.drawable.number_one,R.raw.number_one));
-        numbersWords.add(new Word("otiiko", "two", R.drawable.number_two,R.raw.number_two));
-        numbersWords.add(new Word("tolookosu", "three", R.drawable.number_three,R.raw.number_three));
-        numbersWords.add(new Word("oyyisa", "four", R.drawable.number_four,R.raw.number_four));
-        numbersWords.add(new Word("massokka", "five", R.drawable.number_five,R.raw.number_five));
-        numbersWords.add(new Word("temmokka", "six", R.drawable.number_six,R.raw.number_six));
-        numbersWords.add(new Word("kenekaku", "seven", R.drawable.number_seven,R.raw.number_seven));
-        numbersWords.add(new Word("kawinta", "eight", R.drawable.number_eight,R.raw.number_eight));
-        numbersWords.add(new Word("wo’e", "nine", R.drawable.number_nine,R.raw.number_nine));
-        numbersWords.add(new Word("na’aacha", "ten", R.drawable.number_ten,R.raw.number_ten));
+        numbersWords.add(new Word("lutti", "one", R.drawable.number_one, R.raw.number_one));
+        numbersWords.add(new Word("otiiko", "two", R.drawable.number_two, R.raw.number_two));
+        numbersWords.add(new Word("tolookosu", "three", R.drawable.number_three, R.raw.number_three));
+        numbersWords.add(new Word("oyyisa", "four", R.drawable.number_four, R.raw.number_four));
+        numbersWords.add(new Word("massokka", "five", R.drawable.number_five, R.raw.number_five));
+        numbersWords.add(new Word("temmokka", "six", R.drawable.number_six, R.raw.number_six));
+        numbersWords.add(new Word("kenekaku", "seven", R.drawable.number_seven, R.raw.number_seven));
+        numbersWords.add(new Word("kawinta", "eight", R.drawable.number_eight, R.raw.number_eight));
+        numbersWords.add(new Word("wo’e", "nine", R.drawable.number_nine, R.raw.number_nine));
+        numbersWords.add(new Word("na’aacha", "ten", R.drawable.number_ten, R.raw.number_ten));
 
-        final WordAdapter itemsAdapter = new WordAdapter(this, numbersWords,R.color.category_numbers);
+        final WordAdapter itemsAdapter = new WordAdapter(this, numbersWords, R.color.category_numbers);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.v("Number Activity","Current word is: "+ numbersWords.get(i).toString());
-                mediaPlayer = MediaPlayer.create(NumbersActivity.this,numbersWords.get(i).getmAudioID());
+                Log.v("Number Activity", "Current word is: " + numbersWords.get(i).toString());
+                mediaPlayer = MediaPlayer.create(NumbersActivity.this, numbersWords.get(i).getmAudioID());
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mediaPlayer.stop();
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -19,16 +19,16 @@ public class PhrasesActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         final ArrayList<Word> phrasesWords = new ArrayList<Word>();
-        phrasesWords.add(new Word("minto wuksus", "Where are you going?",-1,R.raw.phrase_where_are_you_going));
-        phrasesWords.add(new Word("tinnә oyaase'nә", "What is your name?",-1,R.raw.phrase_what_is_your_name));
-        phrasesWords.add(new Word("oyaaset...", "My name is.",-1,R.raw.phrase_my_name_is));
-        phrasesWords.add(new Word("michәksәs?", "How are you feeling?",-1,R.raw.phrase_how_are_you_feeling));
-        phrasesWords.add(new Word("kuchi achit", "I’m feeling good",-1,R.raw.phrase_im_feeling_good));
-        phrasesWords.add(new Word("әәnәs'aa?", "Are you coming?",-1,R.raw.phrase_are_you_coming));
-        phrasesWords.add(new Word("hәә’әәnәm", "Yes, I’m coming.",-1,R.raw.phrase_yes_im_coming));
-        phrasesWords.add(new Word("әәnәm", "I’m coming.",-1,R.raw.phrase_im_coming));
-        phrasesWords.add(new Word("yoowutis", "Let’s go.",-1,R.raw.phrase_lets_go));
-        phrasesWords.add(new Word("әnni'nem", "Come here.",-1,R.raw.phrase_come_here));
+        phrasesWords.add(new Word("minto wuksus", "Where are you going?", -1, R.raw.phrase_where_are_you_going));
+        phrasesWords.add(new Word("tinnә oyaase'nә", "What is your name?", -1, R.raw.phrase_what_is_your_name));
+        phrasesWords.add(new Word("oyaaset...", "My name is.", -1, R.raw.phrase_my_name_is));
+        phrasesWords.add(new Word("michәksәs?", "How are you feeling?", -1, R.raw.phrase_how_are_you_feeling));
+        phrasesWords.add(new Word("kuchi achit", "I’m feeling good", -1, R.raw.phrase_im_feeling_good));
+        phrasesWords.add(new Word("әәnәs'aa?", "Are you coming?", -1, R.raw.phrase_are_you_coming));
+        phrasesWords.add(new Word("hәә’әәnәm", "Yes, I’m coming.", -1, R.raw.phrase_yes_im_coming));
+        phrasesWords.add(new Word("әәnәm", "I’m coming.", -1, R.raw.phrase_im_coming));
+        phrasesWords.add(new Word("yoowutis", "Let’s go.", -1, R.raw.phrase_lets_go));
+        phrasesWords.add(new Word("әnni'nem", "Come here.", -1, R.raw.phrase_come_here));
 
         WordAdapter itemsAdapter = new WordAdapter(this, phrasesWords, R.color.category_phrases);
         ListView listView = (ListView) findViewById(R.id.list);
@@ -36,10 +36,34 @@ public class PhrasesActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.v("Phrases Activity","Current word is: "+ phrasesWords.get(i).toString());
+                Log.v("Phrases Activity", "Current word is: " + phrasesWords.get(i).toString());
                 mediaPlayer = MediaPlayer.create(PhrasesActivity.this, phrasesWords.get(i).getmAudioID());
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mediaPlayer.stop();
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
