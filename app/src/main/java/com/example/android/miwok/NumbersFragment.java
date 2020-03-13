f3ac76b18422b7716e807d647db6227ef696b64a
Shonen <shonen1988@gmail.com>
2020-03-13 13:43:22 +0200

    add NumberFragment

---
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -0,0 +1,110 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class NumbersFragment extends Fragment {
+    private MediaPlayer mediaPlayer;
+    private AudioManager mAudioManager;
+    private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+
+    public NumbersFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list,container,false);
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        final ArrayList<Word> numbersWords = new ArrayList<Word>();
+        numbersWords.add(new Word("lutti", "one", R.drawable.number_one, R.raw.number_one));
+        numbersWords.add(new Word("otiiko", "two", R.drawable.number_two, R.raw.number_two));
+        numbersWords.add(new Word("tolookosu", "three", R.drawable.number_three, R.raw.number_three));
+        numbersWords.add(new Word("oyyisa", "four", R.drawable.number_four, R.raw.number_four));
+        numbersWords.add(new Word("massokka", "five", R.drawable.number_five, R.raw.number_five));
+        numbersWords.add(new Word("temmokka", "six", R.drawable.number_six, R.raw.number_six));
+        numbersWords.add(new Word("kenekaku", "seven", R.drawable.number_seven, R.raw.number_seven));
+        numbersWords.add(new Word("kawinta", "eight", R.drawable.number_eight, R.raw.number_eight));
+        numbersWords.add(new Word("wo’e", "nine", R.drawable.number_nine, R.raw.number_nine));
+        numbersWords.add(new Word("na’aacha", "ten", R.drawable.number_ten, R.raw.number_ten));
+
+        final WordAdapter itemsAdapter = new WordAdapter(getActivity(), numbersWords, R.color.category_numbers);
+        ListView listView = (ListView)rootView.findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.v("Number Activity", "Current word is: " + numbersWords.get(i).toString());
+                releaseMediaPlayer();
+                int result = mAudioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mediaPlayer = MediaPlayer.create(getActivity(), numbersWords.get(i).getmAudioID());
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
+            }
+        });
+        return rootView;
+    }
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+            mAudioManager.abandonAudioFocus(audioFocusChangeListener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+}
